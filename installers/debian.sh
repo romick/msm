e0b72c66b9221d7d2aa611483d5d5aaff0d57c24
romick <roman.milovanov@gmail.com>
2015-11-01 16:58:44 +0300

    Changed installer repo to romick

--- installers/debian.sh
+++ installers/debian.sh
@@ -1,4 +1,4 @@
-UPDATE_URL="https://raw.githubusercontent.com/msmhq/msm/master"
+UPDATE_URL="https://raw.githubusercontent.com/romick/msm/master"
 wget -q ${UPDATE_URL}/installers/common.sh -O /tmp/msmcommon.sh
 source /tmp/msmcommon.sh && rm -f /tmp/msmcommon.sh
 
