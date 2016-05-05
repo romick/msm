7d1f567d553d9d80c83a4162d8176606373f9dc1
romick <roman.milovanov@gmail.com>
2016-05-06 03:17:17 +0400

    added "agree to eula" part

--- msm-node/setup.sh
+++ msm-node/setup.sh
@@ -7,6 +7,9 @@ echo $MSM_SSH_PUB_KEY >> ~/.ssh/authorized_keys
 #create minecraft server
 msm server create anyserver
 
+#agree to EULA
+sed -i -- 's/false/true/g' /opt/msm/servers/anyserver/eula.txt
+
 #start minecraft server
 msm anyserver start
 
