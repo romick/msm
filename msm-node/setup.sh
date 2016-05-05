ef4606241345f7a19bef0d97e829a25c883977c8
romick <roman.milovanov@gmail.com>
2016-05-06 02:42:57 +0400

    starting minecraft server by container itself

--- msm-node/setup.sh
+++ msm-node/setup.sh
@@ -4,5 +4,11 @@
 mkdir ~/.ssh
 echo $MSM_SSH_PUB_KEY >> ~/.ssh/authorized_keys
 
+#create minecraft server
+msm create anyserver
+
+#start minecraft server
+msm anyserver start
+
 #start ssh
 /usr/sbin/sshd -D
