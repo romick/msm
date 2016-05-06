19ae87012fcf2d10d90df08c8dfee2964379c3b7
romick <roman.milovanov@gmail.com>
2016-05-06 04:03:00 +0400

    server name is now variable

--- msm-node/setup.sh
+++ msm-node/setup.sh
@@ -4,17 +4,19 @@
 mkdir ~/.ssh
 echo $MSM_SSH_PUB_KEY >> ~/.ssh/authorized_keys
 
+export MSM_SERVER_NAME=${MSM_SERVER_NAME:=anyserver}
+
 #create minecraft server
-msm server create anyserver
+msm server create $MSM_SERVER_NAME
 
 #start minecraft server
-msm anyserver start
+msm $MSM_SERVER_NAME start
 
 #agree to EULA
-sed -i -- 's/false/true/g' /opt/msm/servers/anyserver/eula.txt
+sed -i -- 's/false/true/g' /opt/msm/servers/$MSM_SERVER_NAME/eula.txt
 
 #start minecraft server, again...
-msm anyserver start
+msm $MSM_SERVER_NAME start
 
 #start ssh
 /usr/sbin/sshd -D
