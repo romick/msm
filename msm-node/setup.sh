ca004379ab72868f56690e1ba2434c6441ddc6a1
romick <roman.milovanov@gmail.com>
2016-05-06 08:42:47 +0400

    added maps

--- msm-node/setup.sh
+++ msm-node/setup.sh
@@ -5,9 +5,13 @@ mkdir ~/.ssh
 echo $MSM_SSH_PUB_KEY >> ~/.ssh/authorized_keys
 
 export MSM_SERVER_NAME=${MSM_SERVER_NAME:=anyserver}
+export MSM_MAP_NAME=${MSM_MAP_NAME:=default}
 
 #create minecraft server
 msm server create $MSM_SERVER_NAME
+
+#copy map to server folder
+cp -r /data/global-maps/$MSM_MAP_NAME /opt/msm/servers/$MSM_SERVER_NAME/worldstorage/
 
 #start minecraft server
 msm $MSM_SERVER_NAME start
