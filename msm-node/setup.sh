ae6232eb47101e2a461bac0f9aa7803a0c098357
romick <roman.milovanov@gmail.com>
2016-05-10 01:52:32 +0400

    auto-eula and auto-ram settings

--- msm-node/setup.sh
+++ msm-node/setup.sh
@@ -6,6 +6,7 @@ echo $MSM_SSH_PUB_KEY >> ~/.ssh/authorized_keys
 
 export MSM_SERVER_NAME=${MSM_SERVER_NAME:=anyserver}
 export MSM_MAP_NAME=${MSM_MAP_NAME:=default}
+export MSM_SERVER_RAM=${MSM_SERVER_RAM:=256}
 
 #create minecraft server
 msm server create $MSM_SERVER_NAME
@@ -13,11 +14,17 @@ msm server create $MSM_SERVER_NAME
 #copy map to server folder
 cp -r /data/global-maps/$MSM_MAP_NAME /opt/msm/servers/$MSM_SERVER_NAME/worldstorage/
 
+#set memory to 256M
+echo "msm-ram=$MSM_SERVER_RAM" >> /opt/msm/servers/$MSM_SERVER_NAME/server.properties
+
+#set memory to 256M
+echo "eula=true" >> /opt/msm/servers/$MSM_SERVER_NAME/eula.txt
+
 #start minecraft server
-msm $MSM_SERVER_NAME start
+#msm $MSM_SERVER_NAME start
 
 #agree to EULA
-sed -i -- 's/false/true/g' /opt/msm/servers/$MSM_SERVER_NAME/eula.txt
+#sed -i -- 's/false/true/g' /opt/msm/servers/$MSM_SERVER_NAME/eula.txt
 
 #start minecraft server, again...
 msm $MSM_SERVER_NAME start
